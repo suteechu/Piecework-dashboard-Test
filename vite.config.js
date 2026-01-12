bcf9b5b935d17cbc010bb1964ed80b5a9427ed76
suteechu <96893367+suteechu@users.noreply.github.com>
2026-01-12 17:48:16 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Piecework-dashboard-Test/', // เพิ่มบรรทัดนี้ (ชื่อต้องตรงกับชื่อ Repository)
   server: {
     port: 5173,
     open: true
